b1a6493fdfd7edd69fe13b6461cf32f84e4497e5
dinesh kumar <143687187+Dineshkumarkota@users.noreply.github.com>
2025-07-24 10:49:46 +0000

    final

--- src/index.mjs
+++ src/index.mjs
@@ -9,3 +9,17 @@ menuButton.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   nav.classList.remove("open");
 });
+function updateHeroImage() {
+  const heroImg = document.querySelector(".hero-img img");
+  if (window.innerWidth < 1000) {
+    heroImg.src = "./image-web-3-mobile.jpg";
+  } else {
+    heroImg.src = "./image-web-3-desktop.jpg";
+  }
+}
+
+// Run once on load
+updateHeroImage();
+
+// Also update when screen is resized
+window.addEventListener("resize", updateHeroImage);
